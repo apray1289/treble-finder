664aeb714e77e6685b8dca14de8bd3270ed10415
Sam Howard <samhoward1990@gmail.com>
2019-09-28 09:47:42 -0500

    changed the datatype of experience to a number

--- models/skill.js
+++ models/skill.js
@@ -5,7 +5,7 @@ module.exports = function (sequelize, DataTypes) {
             allowNull: false
         },
         experience: {
-            type: DataTypes.STRING,
+            type: DataTypes.INTEGER,
             allowNull: false
         }
     });
