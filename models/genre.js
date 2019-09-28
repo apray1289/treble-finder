af704768c2171e2509e9ddb9fa834608c1afb406
Sam Howard <samhoward1990@gmail.com>
2019-09-28 15:09:05 -0500

    added genre-seeds.sql updated genre model

--- models/genre.js
+++ models/genre.js
@@ -2,6 +2,12 @@ module.exports = function (sequelize, DataTypes) {
     var Genre = sequelize.define("Genre", {
         name: {
             type: DataTypes.STRING
+        },
+        createdAt: {
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            type: DataTypes.DATE
         }
     });
     return Genre;
