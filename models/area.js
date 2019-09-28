4bdc6c7f547f7e8a7b9f3ed3e73dffb284bcd412
Sam Howard <samhoward1990@gmail.com>
2019-09-28 11:55:35 -0500

    fixed user area association

--- models/area.js
+++ models/area.js
@@ -7,12 +7,5 @@ module.exports = function (sequelize, DataTypes) {
     Area.associate = function (models) {
         Area.hasMany(models.User);
     };
-    Area.associate = function (models) {
-        Area.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
     return Area;
 }; 
