c3b2b6a752f1e4d2c60614de0a574b4c9091f68f
Fawzi <alami42@live.com>
2019-09-29 15:53:12 -0500

    added login code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+var pass = require("../public/js/password.js");
 
 module.exports = function(app) {
   // Get all Users
@@ -27,6 +28,33 @@ module.exports = function(app) {
       res.json(dbUser);
     });
   });
+
+  // login
+  app.post("/api/login", function(req,res){
+    var plainPassword = req.body.password;
+    db.User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(function(dbUser){
+      // verify passwords match
+      pass.comparePassword(plainPassword, dbUser.password, function(err,match){
+        console.log('match', match);
+        if (match) {
+          res.render('profile', dbUser, function(err,html){
+            if (err) {
+              res.status(404).send(err);
+            } 
+            res.send(html);
+          });
+        } else {
+          res.json({
+            error: 'Invalid email/password!'
+          });
+        }
+      })
+    });
+  })
 
   // Delete an User by id
   app.delete("/api/Users/:id", function(req, res) {
