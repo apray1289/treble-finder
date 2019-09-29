768f8b04fa3a27b77afb1829a9e790e7c808f8ef
Fawzi <alami42@live.com>
2019-09-29 16:12:26 -0500

    Ah! Fix syntax errors!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -64,130 +64,128 @@ module.exports = function(app) {
       res.json(dbUser);
     });
   });
-};
 
-// register
-app.post("/api/newuser", function(req, res) {
-  var plainPassword = req.body.password;
-  db.User.count({
-    where: {
-      email: req.body.email
-    }
-  }).then(function(count) {
-    if (count !== 0) {
-      // duplicate email address
-      res.json({
-        error: "Email address already exists!"
-      });
-    } else {
-      db.User.create({}).then(function(dbuser) {
-        pass.cryptPassword(plainPassword, function(err, encrypted) {
-          var areas = [];
-          if (req.body.areas) {
-            areas = req.body.areas.split(",");
-          }
-          var skills = [];
-          if (req.body.talents) {
-            skills = req.body.talents.split(",");
-          }
-          var genres = [];
-          if (req.body.genres) {
-            genres = req.body.genres.split(",");
-            console.log(req.body.genres);
-            console.log(genres);
-          }
-          // Create the user
-          const newUser = db.User.create({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            email: req.body.email,
-            password: encrypted,
-            phone: req.body.phone,
-            bio: req.body.bio,
-            soundLink: req.body.soundLink
-          })
-            .then(function(dbUser) {
-              // Add areas
-              if (areas.length) {
-                dbUser
-                  .addAreas(areas, {
-                    where: {
-                      UserId: dbUser.id
-                    }
-                  })
-                  .then(function() {
-                    console.log("areas added");
-                  });
-              }
-
-              // Add skills
-              if (skills.length) {
-                dbUser
-                  .addSkills(skills, {
-                    where: {
-                      UserId: dbUser.id
-                    }
-                  })
-                  .then(function() {
-                    console.log("skills added");
-                  });
-              }
-
-              // Add genres
-              if (genres.length) {
-                dbUser
-                  .addGenres(genres, {
-                    where: {
-                      UserId: dbUser.id
-                    }
-                  })
-                  .then(function() {
-                    console.log("genres added");
-                    res.json(dbUser);
-                  });
-              }
-            })
-            .catch(function(err) {
-              // handle error;
-              res.json({
-                error: err.message
-              });
-            });
-        });
-      });
-    }
-  });
-});
-
-// find musician with specific area, skill, and genre requirements
-app.post('/api/finduser', function (req,res){
-
-  var areas = req.body.areas.split(",");
-  var skills = req.body.skills.split(",");
-  var genres = req.body.genres.split(",");
-  db.User.findAll({
-    include: [
-      {
-        model: db.Area,
-        where: {
-          name: areas
-        }
-      },
-      {
-        model: db.Skill,
-        where: {
-          name: skills
-        }
-      },
-      {
-        model: db.Genre,
-        where: {
-          name: genres
-        }
+  // register
+  app.post("/api/newuser", function(req, res) {
+    var plainPassword = req.body.password;
+    db.User.count({
+      where: {
+        email: req.body.email
       }
-    ]
-  }).then(function (dbUsers){
-    res.json(dbUsers);
-  })
+    }).then(function(count) {
+      if (count !== 0) {
+        // duplicate email address
+        res.json({
+          error: "Email address already exists!"
+        });
+      } else {
+        db.User.create({}).then(function(dbuser) {
+          pass.cryptPassword(plainPassword, function(err, encrypted) {
+            var areas = [];
+            if (req.body.areas) {
+              areas = req.body.areas.split(",");
+            }
+            var skills = [];
+            if (req.body.talents) {
+              skills = req.body.talents.split(",");
+            }
+            var genres = [];
+            if (req.body.genres) {
+              genres = req.body.genres.split(",");
+              console.log(req.body.genres);
+              console.log(genres);
+            }
+            // Create the user
+            const newUser = db.User.create({
+              firstName: req.body.firstName,
+              lastName: req.body.lastName,
+              email: req.body.email,
+              password: encrypted,
+              phone: req.body.phone,
+              bio: req.body.bio,
+              soundLink: req.body.soundLink
+            })
+              .then(function(dbUser) {
+                // Add areas
+                if (areas.length) {
+                  dbUser
+                    .addAreas(areas, {
+                      where: {
+                        UserId: dbUser.id
+                      }
+                    })
+                    .then(function() {
+                      console.log("areas added");
+                    });
+                }
 
-});
+                // Add skills
+                if (skills.length) {
+                  dbUser
+                    .addSkills(skills, {
+                      where: {
+                        UserId: dbUser.id
+                      }
+                    })
+                    .then(function() {
+                      console.log("skills added");
+                    });
+                }
+
+                // Add genres
+                if (genres.length) {
+                  dbUser
+                    .addGenres(genres, {
+                      where: {
+                        UserId: dbUser.id
+                      }
+                    })
+                    .then(function() {
+                      console.log("genres added");
+                      res.json(dbUser);
+                    });
+                }
+              })
+              .catch(function(err) {
+                // handle error;
+                res.json({
+                  error: err.message
+                });
+              });
+          });
+        });
+      }
+    });
+  });
+
+  // find musician with specific area, skill, and genre requirements
+  app.post("/api/finduser", function(req, res) {
+    var areas = req.body.areas.split(",");
+    var skills = req.body.skills.split(",");
+    var genres = req.body.genres.split(",");
+    db.User.findAll({
+      include: [
+        {
+          model: db.Area,
+          where: {
+            name: areas
+          }
+        },
+        {
+          model: db.Skill,
+          where: {
+            name: skills
+          }
+        },
+        {
+          model: db.Genre,
+          where: {
+            name: genres
+          }
+        }
+      ]
+    }).then(function(dbUsers) {
+      res.json(dbUsers);
+    });
+  });
+};
